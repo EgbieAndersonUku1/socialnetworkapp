e523ec2a8930b00fab5e7a41ad2b17dc442ba004
EgbieAndersonUku1 <jayunderwood2011@hotmail.com>
2019-05-23 02:22:05 +0100

    Update run.py

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from flask_script import Manager, Server
 
-from app import create_app
+from application import create_app
 
 app = create_app()
 
