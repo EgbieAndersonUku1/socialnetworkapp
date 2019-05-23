33526c6c837c32c1fa7b87cadfaab5cb18eae7da
EgbieAndersonUku1 <jayunderwood2011@hotmail.com>
2019-05-23 02:11:00 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ SECRET_KEY = "nXr2Z5lidzm-PeVFqy33zwKxEORmxlJMlkItGE-UiMq7Qs2vdWXaXcxfuKwfQ"
 DEBUG=True
 HOSTNAME = "http://127.0.0.1:5000"
 MONGODB_DB = "name of database here no spaces"
-UPLOADED_FOLDER = "/name/of/folder/here/static/images"
+UPLOADED_FOLDER = "/name/of/folder/here/and/the/path/to/static/images"
 STATIC_IMAGE_URL = "images"
 
